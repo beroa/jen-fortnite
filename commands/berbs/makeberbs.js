9a4b8d4341ebd43fd7d3fcda413725e90e0cf468
beroa <alanberotti@gmail.com>
2024-11-14 11:35:38 -0500

    add luke

--- commands/berbs/makeberbs.js
+++ commands/berbs/makeberbs.js
@@ -9,7 +9,7 @@ import {
   ATTEND_LIMIT,
 } from "./db_events.js";
 
-const AUTHORIZED_USER_IDS = ['107298762174541824', '325463238043238402'];
+const AUTHORIZED_USER_IDS = ['107298762174541824', '325463238043238402', '283954675064242177'];
 
 export const command = {
   data: new SlashCommandBuilder()
